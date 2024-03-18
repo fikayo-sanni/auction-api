9252cb1b7e4d239867f37d5c5856d170f32eaf5f
Sanni Oluwafikayo <fikayo.iuris@gmail.com>
2024-03-18 07:27:02 +0100

    chore: setup initial ResponseCodes and ResponseMessages enum

---
+++ src/ constants/ResponseMessages.ts
@@ -0,0 +1,10 @@
+export enum ResponseMessages {
+  SOMETHING_WENT_WRONG = 'something_went_wrong',
+  ACCESS_DENIED = 'access_denied',
+  NOT_FOUND = 'not_found_error',
+  INVALID_REQUEST_DATA = 'invalid_request_data',
+  EXPIRED = 'resource_has_expired',
+  NOT_ALLOWED = 'operation_not_allowed',
+  UNAUTHORIZED = 'unauthorized',
+  FORMAT_NOT_SUPPORTED = 'format_not_supported',
+}
