b373d14b90fcb883cf9d5fe9c63d6946b7399565
Sanni Oluwafikayo <fikayo.iuris@gmail.com>
2024-03-19 08:09:27 +0100

    feat: add userservice to user module

--- src/http/api/v1/users/users.module.ts
+++ src/http/api/v1/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { SharedModule } from 'src/shared/shared.module';
+import { UsersService } from './services/users.service';
 
 @Module({
   imports: [PrismaModule, SharedModule],
   controllers: [],
-  providers: [],
-  exports: [],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
