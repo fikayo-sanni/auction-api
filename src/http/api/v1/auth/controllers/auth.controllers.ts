b726ab22c24162d69174b3947eea91cd3ec1a675
Sanni Oluwafikayo <fikayo.iuris@gmail.com>
2024-03-20 09:27:27 +0100

    feat: add signin controller

--- src/http/api/v1/auth/controllers/auth.controllers.ts
+++ src/http/api/v1/auth/controllers/auth.controllers.ts
@@ -1,7 +1,10 @@
 import { BaseAppController } from 'src/http/api/base/base.controller';
+import { Request, Response } from 'express';
 import { AuthService } from '../services/auth.service';
 import { Body, Controller, Post, Req, Res } from '@nestjs/common';
 import { AuthSignUpDto } from '../dtos/auth.signup.dto';
+import { CustomValidationPipe } from 'src/shared/pipes/customValidation.pipe';
+import { AuthSignInDto } from '../dtos/auth.signin.dto';
 
 @Controller('api/v1/auth')
 export class AuthController extends BaseAppController {
@@ -17,6 +20,17 @@ export class AuthController extends BaseAppController {
   ) {
     const result = await this.authService.signUp(user);
 
-    return this.getHttpResponse().setDataWithKey('data', result).send(req, res);
+    return this.getHttpResponse().setDataWithKey('data', result).send(res);
+  }
+
+  @Post('login')
+  public async signin(
+    @Body(new CustomValidationPipe()) data: AuthSignInDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const result = await this.authService.signIn(data);
+
+    return this.getHttpResponse().setAuthDataWithKey('data', result).send(res);
   }
 }
