f64cfd9731d87616e5ba8b656a3feed70b97c9af
Sanni Oluwafikayo <fikayo.iuris@gmail.com>
2024-03-23 21:11:20 +0100

    feat: completed module files definition with controllers and providers

--- src/http/api/v1/auth/auth.module.ts
+++ src/http/api/v1/auth/auth.module.ts
@@ -1,9 +1,16 @@
 import { Global, Module } from '@nestjs/common';
+import { AuthController } from './controllers/auth.controllers';
+import { UsersService } from '../users/services/users.service';
+import { PrismaService } from 'src/prisma/services/prisma.service';
+import { AppLogger } from 'src/shared/utils/AppLogger';
+import { AuthService } from './services/auth.service';
+import { JwtService } from '@nestjs/jwt';
 
-const providers = [];
+const providers = [AuthService];
 
 @Module({
-  providers: [...providers],
+  controllers: [AuthController],
+  providers: [...providers, UsersService, PrismaService, AppLogger, JwtService],
   exports: providers,
 })
 @Global()
